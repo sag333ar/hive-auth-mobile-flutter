4c72f8bed00be06db1103f26a7e0cee6e59ef9f7
Sagar <sagar.devices@gmail.com>
2023-08-26 22:52:04 +0530

    Android changes

---
+++ android/app/src/main/kotlin/com/hiveauth/authsigner/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.hiveauth.authsigner
+
+import io.flutter.embedding.android.FlutterActivity
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.webkit.JavascriptInterface
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
+import androidx.webkit.WebViewAssetLoader
+import com.google.gson.Gson
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    var webView: WebView? = null
+    var result: MethodChannel.Result? = null
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        if (webView == null) {
+            setupView()
+        }
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.hiveauth.hiveauthsigner/bridge"
+        ).setMethodCallHandler { call, result ->
+            this.result = result
+            val publicKey = call.argument<String?>("publicKey")
+            val privateKey = call.argument<String?>("privateKey")
+            val memo = call.argument<String>("memo")
+            if (call.method == "getProofOfKey" && publicKey != null && privateKey != null && memo != null) {
+                webView?.evaluateJavascript("getProofOfKey('$privateKey', '$publicKey', '$memo');", null)
+            }
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupView() {
+        val params = FrameLayout.LayoutParams(0, 0)
+        webView = WebView(activity)
+        val decorView = activity.window.decorView as FrameLayout
+        decorView.addView(webView, params)
+        webView?.visibility = View.GONE
+        webView?.settings?.javaScriptEnabled = true
+        webView?.settings?.domStorageEnabled = true
+//        webView?.webChromeClient = WebChromeClient()
+        WebView.setWebContentsDebuggingEnabled(true)
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build()
+        val client: WebViewClient = object : WebViewClient() {
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(request.url)
+            }
+
+            override fun shouldInterceptRequest(
+                view: WebView,
+                url: String
+            ): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(Uri.parse(url))
+            }
+        }
+        webView?.webViewClient = client
+        webView?.addJavascriptInterface(WebAppInterface(this), "Android")
+        webView?.loadUrl("https://appassets.androidplatform.net/assets/index.html")
+    }
+}
+
+class WebAppInterface(private val mContext: Context) {
+    @JavascriptInterface
+    fun postMessage(message: String) {
+        val main = mContext as? MainActivity ?: return
+        val gson = Gson()
+        val dataObject = gson.fromJson(message, JSEvent::class.java)
+        when (dataObject.type) {
+            JSBridgeAction.GET_PROOF_OF_KEY.value -> {
+                main.result?.success(message)
+            }
+        }
+    }
+}
+
+data class JSEvent(
+    val type: String,
+    val error: String,
+    val data: String,
+)
+
+enum class JSBridgeAction(val value: String) {
+    GET_PROOF_OF_KEY("getProofOfKey"),
+}
+
